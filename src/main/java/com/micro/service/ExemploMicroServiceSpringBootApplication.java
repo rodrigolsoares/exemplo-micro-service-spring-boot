2bf41c847453179b29e50999073420b3832ac4a4
RODRIGO-PC\Rodrigo <rodrigo.soares@bookhair.com.br>
2019-07-26 19:24:20 -0300

    Criando exemplo

---
+++ src/main/java/com/micro/service/ExemploMicroServiceSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.micro.service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExemploMicroServiceSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExemploMicroServiceSpringBootApplication.class, args);
+	}
+
+}
